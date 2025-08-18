a2638d8543f027656746598ae8d8f1d7f4d31fa4
pradeepmurthy1992 <pradeepmurthy8@gmail.com>
2025-08-18 12:36:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ⬇️ replace `pradhu-site` with YOUR repo name if this is a project page.
-// If you use a user/org page repo named <username>.github.io, set base: '/' instead.
 export default defineConfig({
-  base: '/pradhu-site-test/',
+  base: '/pradhu-site-test/', // <-- repo name for GitHub Pages project site
   plugins: [react()],
 })
